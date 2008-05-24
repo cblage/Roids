01c78931b8b0cb12e41506b59c4c4be6143d5c23
Carlos Brito Lage <carlos@carloslage.net>
2008-05-24 22:34:47 +0000

    Ooops, esquecime dum ficheiro para o commit anterior.

---
+++ cglib.dev/asteroids/src/GameManager.cpp
@@ -0,0 +1,96 @@
+#include "GameManager.h"
+
+namespace asteroids {
+	GameManager::GameManager(std::string id, MyApp *application) : ParticleManager(id), _application(application) {
+		_shipsLeft = cg::Properties::instance()->getInt("SHIP_LIFES");
+		_asteroidsLeft = 0;
+	}
+	GameManager::~GameManager() {
+		ParticleManager::~ParticleManager();
+	}
+	
+	void GameManager::createEntities() {
+		changeLevel(0);
+		beginLevel();
+	}
+	
+
+	void GameManager::resetGame() {
+		_shipsLeft = cg::Properties::instance()->getInt("SHIP_LIFES");
+		
+		//make sure we have a clean slate - no new particles are going to be added
+		//and we dont want duplicates in the deleted ones
+		_newParticles.clear();
+		_deletedParticles.clear();
+		
+		std::vector<Particle *> particles = getParticles();
+		for(std::vector<Particle*>::size_type i = 0; i < particles.size(); i++) {
+			//cant use this because asteroids would generate new ones
+			//particles[i]->destroy();
+			destroyParticle(particles[i]->getId());
+		}
+		changeLevel(0);
+	}
+
+	void GameManager::addParticle(Particle * p) {
+		if(p->getParticleType() == 'a') {
+			_asteroidsLeft++;
+		}
+		ParticleManager::addParticle(p);
+	}
+
+	void GameManager::destroyParticle(std::string id) {
+		Particle * destroyedParticle = getParticle(id);
+		char pType = destroyedParticle->getParticleType();
+		
+		switch(pType) {
+			case 'a':
+				addScore(destroyedParticle->getMass()/10);
+				_asteroidsLeft--;
+				break;
+			case 's':
+				addScore(-1000);
+				if (_shipsLeft > 0) {
+					_shipsLeft--;
+					createShip(); 
+				}
+				break;
+		}
+		ParticleManager::destroyParticle(id);
+	}
+	void GameManager::createLaserShot(cg::Vector2d position, double radiansRotation, cg::Vector2d velocity, double degreesRotation) {
+		addScore(-10);
+		ParticleManager::createLaserShot(position, radiansRotation, velocity, degreesRotation);
+	}
+	void GameManager::beginLevel() {
+		if(_currentLevel == 0) { //level 0 = demo level
+			createAsteroids(cg::Properties::instance()->getInt("DEMO_ASTEROIDS"));
+		}
+		
+	}
+	void GameManager::finishLevel() {
+		
+	}
+	
+	void GameManager::advanceLevel() {
+		_currentLevel++;
+	}
+
+	void GameManager::changeLevel(unsigned int level) {
+		_currentLevel = level;
+	}
+	unsigned int GameManager::getCurrentLevel() {
+		return _currentLevel;
+	}
+	int GameManager::getCurrentScore() {
+		return _currentScore;
+	}
+	void GameManager::setScore(int score) {
+		_currentScore = score;
+	}
+	void GameManager::addScore(int score) {
+		_currentScore += score;
+	}
+
+
+}
---
+++ cglib.dev/asteroids/src/GameManager.h
@@ -0,0 +1,76 @@
+#pragma once
+#ifndef GAME_MANAGER_H
+#define GAME_MANAGER_H
+#pragma message("GameManager is included")
+#include <sstream>
+#include <string>
+#include <vector>
+#include "cg/cg.h"
+#include "ParticleManager.h"
+#include "ApplicationState.h"
+
+
+namespace asteroids {
+	class MyApp;
+
+	class GameManager : public ParticleManager
+	{
+	private:
+		int _currentScore;
+		unsigned int _currentLevel, _asteroidsLeft, _shipsLeft;
+		MyApp * _application;
+
+	public:
+		GameManager(std::string id, MyApp *application);
+		~GameManager();
+		void createEntities();
+		void addParticle(Particle *p);
+		void destroyParticle(std::string id);
+		void createLaserShot(cg::Vector2d position, double radiansRotation, cg::Vector2d velocity, double degreesRotation);
+		void resetGame();
+		void beginLevel();
+		void finishLevel();
+		void advanceLevel();
+		void changeLevel(unsigned int level);
+		unsigned int getCurrentLevel();
+		int getCurrentScore();
+		void setScore(int score);
+		void addScore(int score);
+	};
+
+
+#ifndef MY_APP_H
+#define MY_APP_H
+	class StarSphere;
+	class MyCamera;
+	class GameManager;
+	class ScreenManager;
+	class ApplicationState;
+	class Screen;
+
+
+
+	class MyApp : public cg::Application {
+	private:
+		ApplicationState * _state;
+		ScreenManager * _screenManager;
+		GameManager * _gameManager;
+	public:
+		MyApp();
+		~MyApp();
+		void createEntities();
+		void onUpdate();
+		void onDisplay();
+		void changeState(ApplicationState * state);
+		ApplicationState * getState();
+		void onKeyPressed(unsigned char key);
+		void pause();
+		void quit();
+		void addScreen(Screen * s);
+		void removeScreen(Screen * s);
+	};
+#endif
+
+}
+
+#endif

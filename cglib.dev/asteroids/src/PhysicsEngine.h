c5d96b742b862b41472c902922e5fea3e98f4eed
Carlos Brito Lage <carlos@carloslage.net>
2008-07-09 12:13:13 +0000

    Merging MacOSX branch back into trunk.

--- cglib.dev/asteroids/src/PhysicsEngine.h
+++ cglib.dev/asteroids/src/PhysicsEngine.h
@@ -1,3 +1,23 @@
+/*
+ Roids!
+ Copyright (C) 2008 Carlos Lage, Tiago Coelho, Eduardo Castanho
+
+ This file is part of Roids!.
+
+ Roids! is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Roids! is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Roids!; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 #pragma once
 #ifndef PHYSICS_ENGINE_H
 #define PHYSICS_ENGINE_H
@@ -15,7 +35,7 @@ namespace  asteroids{
 	class PhysicsObject;
 
 	class PhysicsEngine{
-	public:
+	private:
 		cg::Vector2d _position, _previousPosition;
 		cg::Vector2d _velocity, _previousVelocity;
 		double _acelFactor;

d87511eda58589f590db04196cb23539f3450e05
Carlos Brito Lage <carlos@carloslage.net>
2008-07-09 11:27:21 +0000

    Adding GPL license information.

--- Xcode/PhysicsEngine.h
+++ Xcode/PhysicsEngine.h
@@ -1,3 +1,23 @@
+/*
+ Roids!
+ Copyright (C) 2008 Carlos Lage, Tiago Coelho, Eduardo Castanho
+
+ This file is part of Roids!.
+
+ Roids! is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Roids! is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Roids!; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 #pragma once
 #ifndef PHYSICS_ENGINE_H
 #define PHYSICS_ENGINE_H
